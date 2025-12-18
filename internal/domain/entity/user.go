707d60abfb36ae75c06fc7b4dc4c8aa7d5854b8e
William Luisan <williamluisan@gmail.com>
2025-12-18 14:30:48 +0000

    feat: (WIP) user

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -1,5 +1,7 @@
 package entity
 
+import "github.com/google/uuid"
+
 type User struct {
 	ID	uuid.UUID
 	FirstName	string
@@ -7,4 +9,23 @@ type User struct {
 	Email	string
 	Password string
 	CreatedAt	time.Time
+}
+
+// DTO (Data Transfer Object)
+type UserInput struct {
+	FirstName	string
+	LastName	string
+	Email		string
+	Password	string
+}
+
+func NewUser(id uuid.UUID, firstName, lastName, email, password string) *User {
+	return &User{
+		ID:	id,
+		FirstName: firstName,
+		LastName: lastName,
+		Email: email,
+		Password: password,
+		CreatedAt: time.Now()
+	}
 }
